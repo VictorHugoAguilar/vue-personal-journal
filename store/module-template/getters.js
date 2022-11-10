604a8e8b656f2c969bcdc6f7111a84cbb170924f
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-11-10 14:40:35 +0100

    add auth module

--- store/module-template/getters.js
+++ store/module-template/getters.js
@@ -1,3 +1,3 @@
-// export const myGetters = async ( state ) => {
+// export const myGetters = ( state ) => {
 
 // };
---
+++ views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+
+    <h1>Journal</h1>
+
+    <button @click="goToDaybook" class="btn btn-primary">Primary</button>
+    <button @click="goToDaybook" class="btn btn-secondary">Secondary</button>
+    <button @click="goToDaybook" class="btn btn-success">Success</button>
+    <button @click="goToDaybook" class="btn btn-danger">Danger</button>
+
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+  },
+  methods: {
+    goToDaybook() {
+      this.$router.push({ name: 'no-entry' })
+    }
+  }
+}
+</script>
