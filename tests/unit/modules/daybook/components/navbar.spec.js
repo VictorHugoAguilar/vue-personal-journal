3f11ef05025b7459ac6393c730fc646c857e4c48
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-11-12 12:21:10 +0100

    add router mock

--- tests/unit/modules/daybook/components/navbar.spec.js
+++ tests/unit/modules/daybook/components/navbar.spec.js
@@ -3,6 +3,14 @@ import { shallowMount } from "@vue/test-utils";
 import Navbar from "@/modules/daybook/components/Navbar.vue";
 import createVuexStore from "../../../mock-data/mock-store";
 
+const mockRouter = {
+  push: jest.fn(),
+}
+
+jest.mock('vue-router', () => ({
+  useRouter: () => mockRouter, 
+}))
+
 describe("Pruebas en el Navbar components", () => {
   const store = createVuexStore({
     user: {
@@ -20,7 +28,28 @@ describe("Pruebas en el Navbar components", () => {
         plugins: [store],
       },
     });
-
+    
     expect(wrapper.html()).toMatchSnapshot();
   });
+
+  test("click en logout, debe de cerrar la sesión y redireccionar", async () => {
+    const wrapper = shallowMount(Navbar, {
+      global: {
+        plugins: [store],
+      },
+    });
+    await wrapper.find('button').trigger('click')
+
+    expect(mockRouter.push).toHaveBeenCalledTimes(1)
+    expect(mockRouter.push).toHaveBeenCalledWith({"name": "login"})
+
+    expect(store.state.auth).toEqual({
+      user: null,
+      idToken: null,
+      refreshToken: null,
+      status: 'not-authenticated'
+    })
+
+  });
+
 });
